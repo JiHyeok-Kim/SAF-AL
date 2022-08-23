af60885e3f80f1c903a8aa774c7526eaff4b9816
JiHyeok-Kim <109517003+JiHyeok-Kim@users.noreply.github.com>
2022-08-23 22:22:08 +0900

    220822

---
+++ 220822 - Union-Find/알고리즘1번_광주_6반_김지혁.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+	int y, x;
+};
+
+int map[100][100];
+int H, W;
+int R, C;
+int by, bx;
+int n;
+int directY[] = { -3, -3, -2, -2, 2, 2, 3, 3 };
+int directX[] = { -2, 2, -3, 3, -3, 3, -2, 2 };
+int used[100][100];
+int ans;
+int flag;
+
+queue<Node> q;
+
+void moveSang(int by, int bx)
+{
+	q.push({ by, bx });
+	while (!q.empty())
+	{
+		Node now = q.front();
+		q.pop();
+
+		for (int i = 0; i < 8; i++)
+		{
+			int ny = now.y + directY[i];
+			int nx = now.x + directX[i];
+			if (ny < 0 || nx < 0 || ny >= H || nx >= W) continue;
+			if (map[ny][nx] == 1) continue;
+			if (used[ny][nx] == 1) continue;
+			used[ny][nx] = 1;
+			ans++;
+			if (map[ny][nx] == 2)
+			{
+				cout << ans;
+				flag = 1;
+			}
+			q.push({ ny, nx });
+		}
+	}
+
+	return;
+}
+
+int main()
+{
+	cin >> H >> W;
+	cin >> R >> C;
+	cin >> by >> bx;
+	cin >> n;
+	map[R][C] = 1;
+	map[by][bx] = 2;
+	for (int i = 0; i < n; i++)
+	{
+		int y, x;
+		cin >> y >> x;
+		map[y][x] = 1;
+	}
+
+	moveSang(by, bx);
+	if (flag == 0) cout << -1;
+
+	return 0;
+}
---
+++ 220822 - Union-Find/알고리즘2번_광주_6반_김지혁.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string str;
+string bucket[500];
+int last;
+
+int main()
+{
+	cin >> str;
+	int cnt = 0;
+	for (int i = 1; i <= str.length(); i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			bucket[cnt] = str.substr(j - 1, str.length() + 1 - i);
+			cnt++;
+		}
+	}
+	for (int i = 1; i <= str.length(); i++)
+	{
+		last += i;
+	}
+
+	for (int i = 1; i < last; i++)
+	{
+		int start1 = 0;
+		while (1)
+		{
+			int ret1 = str.find(bucket[i], start1);
+			if (ret1 == -1) break;
+			int len = bucket[i].length();
+			int start2 = ret1 + len;
+			int ret2 = str.find(bucket[i], start2);
+			if (ret1 + len == ret2)
+			{
+				cout << len;
+				return 0;
+			}
+			start1 = ret1 + len;
+		}
+	}
+	cout << 0;
+
+	return 0;
+}
